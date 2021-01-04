59e13dfd44a49e0312e43f0a76aac8ec96f64c43
Stackbit <support@stackbit.com>
2021-01-04 19:15:27 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -42,6 +42,7 @@ export default class HeroSection extends React.Component {
                   </div>
                 </div>
               </div>
+              <h1
             </section>
         );
     }
